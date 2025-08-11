2182c3dd403fc3bd631546ee7d2320a17dd596ea
Morten Krogh <morten.krogh@bionamic.io>
2025-08-11 19:54:45 +0200

    iota test

--- src/step-mappings/iota.test.js
+++ src/step-mappings/iota.test.js
@@ -1,6 +1,7 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 import {
+	fill_state_array_with_byte,
 	get_bit_in_state_array,
 	is_zero_state_array,
 	make_state_array,
@@ -27,7 +28,7 @@ test("Round zero", (_t) => {
 	assert(!get_bit_in_state_array(state_array, 2, 4, 0));
 });
 
-test("Round zero with a bit set", (_t) => {
+test("Round zero with one bit set", (_t) => {
 	const state_array = make_state_array();
 	set_bit_in_state_array(state_array, 0, 0, 0, true);
 	do_iota(state_array, 0);
@@ -35,37 +36,21 @@ test("Round zero with a bit set", (_t) => {
 	assert(is_zero_state_array(state_array));
 });
 
-// test("Some ones", (_t) => {
-// 	const state_array = make_state_array();
-// 	const scratch_space = make_state_array();
-// 	set_bit_in_state_array(state_array, 0, 0, 0, true);
-// 	set_bit_in_state_array(state_array, 1, 0, 5, true);
-// 	set_bit_in_state_array(state_array, 3, 4, 23, true);
-// 	set_bit_in_state_array(state_array, 3, 4, 25, true);
-// 	do_pi(state_array, scratch_space);
-// 	assert(!is_zero_state_array(state_array));
-// 	assert(get_bit_in_state_array(state_array, 0, 0, 0));
-// 	assert(!get_bit_in_state_array(state_array, 0, 0, 1));
-// 	assert(!get_bit_in_state_array(state_array, 1, 0, 5));
-// 	assert(get_bit_in_state_array(state_array, 0, 2, 5));
-// 	assert(!get_bit_in_state_array(state_array, 0, 2, 4));
-// 	assert(get_bit_in_state_array(state_array, 4, 3, 23));
-// 	assert(!get_bit_in_state_array(state_array, 4, 3, 24));
-// 	assert(get_bit_in_state_array(state_array, 4, 3, 25));
-// 	assert(!get_bit_in_state_array(state_array, 3, 4, 23));
-// 	assert(!get_bit_in_state_array(state_array, 2, 4, 25));
-// });
-
-// test("All one", (_t) => {
-// 	const state_array = make_state_array();
-// 	const scratch_space = make_state_array();
-// 	fill_state_array_with_byte(state_array, 255);
-// 	do_pi(state_array, scratch_space);
-// 	for (let x = 0; x < 5; x++) {
-// 		for (let y = 0; y < 5; y++) {
-// 			for (let z = 0; z < 64; z++) {
-// 				assert(get_bit_in_state_array(state_array, x, y, z));
-// 			}
-// 		}
-// 	}
-// });
+test("Round ten with all ones", (_t) => {
+	const state_array = make_state_array();
+	fill_state_array_with_byte(state_array, 255);
+	const i_round = 10;
+	do_iota(state_array, i_round);
+	assert(get_bit_in_state_array(state_array, 1, 0, 0));
+	assert(get_bit_in_state_array(state_array, 1, 1, 1));
+	assert(get_bit_in_state_array(state_array, 0, 1, 2));
+	assert(get_bit_in_state_array(state_array, 4, 4, 12));
+	assert(!get_bit_in_state_array(state_array, 0, 0, 0));
+	assert(get_bit_in_state_array(state_array, 0, 0, 1));
+	assert(!get_bit_in_state_array(state_array, 0, 0, 3));
+	assert(get_bit_in_state_array(state_array, 0, 0, 7));
+	assert(!get_bit_in_state_array(state_array, 0, 0, 15));
+	assert(!get_bit_in_state_array(state_array, 0, 0, 31));
+	assert(get_bit_in_state_array(state_array, 0, 0, 63));
+	assert(get_bit_in_state_array(state_array, 0, 0, 40));
+});
